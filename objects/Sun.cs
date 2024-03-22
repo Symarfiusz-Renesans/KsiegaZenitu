c4b05eaf8ef748de33e391e629bd68f38ff7a179
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-03-22 11:56:01 +0100

    wcześniej niezapisane zmiany do wcześniejszego komita

--- objects/Sun.cs
+++ objects/Sun.cs
@@ -3,15 +3,19 @@ using System;
 
 public partial class Sun : Node3D
 {
-	Transform3D transform = new Transform3D();
-	Vector3 axis = new Vector3(1,0,0);
-	[Export] const float rotation = 30f;
+	[Export] Area3D clickablaArea;
 	public override void _Ready(){
 		this.Scale = new Vector3(4,4,4);
+		clickablaArea = this.GetNode<Area3D>("ClickableArea");
+		clickablaArea.Connect("input_event",Callable.From(() => ));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta){
 		RotateY(0.1f * (float)delta);
+	}
+
+	private void onClick(){
+
 	}
 }
