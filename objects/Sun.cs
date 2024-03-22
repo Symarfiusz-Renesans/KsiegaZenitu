5bc6958f95283cb6677bafe90dea7e703232b2c6
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-03-22 13:51:47 +0100

    prace nad obiektem Sun
    1) Obiekt wie, kiedy jest klikany.
    2) Obiekt wysyła własny sygnał przy kliknięciu

--- objects/Sun.cs
+++ objects/Sun.cs
@@ -3,11 +3,13 @@ using System;
 
 public partial class Sun : Node3D
 {
+	[Signal] public delegate void TheSunWasClickedEventHandler();
+
 	[Export] Area3D clickablaArea;
 	public override void _Ready(){
 		this.Scale = new Vector3(4,4,4);
 		clickablaArea = this.GetNode<Area3D>("ClickableArea");
-		clickablaArea.Connect("input_event",Callable.From(() => ));
+		//clickablaArea.Connect("input_event",Callable.From(() => ));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +17,9 @@ public partial class Sun : Node3D
 		RotateY(0.1f * (float)delta);
 	}
 
-	private void onClick(){
-
+	private void OnArea3dInputEvent(Node camera, InputEvent e, Vector3 position, Vector3 normal, int shape_idx){
+		if(e is InputEventMouseButton){
+			EmitSignal(SignalName.TheSunWasClicked);
+		}
 	}
 }
