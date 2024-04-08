263eb7d3c5c1b9c6b838d39ee84cbc995c97530e
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-04-08 20:09:25 +0200

    utworzenie działającego menuPauzy

--- ui/PauseMenuUI.cs
+++ ui/PauseMenuUI.cs
@@ -3,13 +3,54 @@ using System;
 
 public partial class PauseMenuUI : CanvasLayer
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	[Signal] public delegate void SaveProgressEventHandler();
+	private Button ContinueButton;
+	private Button SaveButton;
+	private Button SettingsButton;
+	private Button ExitButton;
+	private MarginContainer MainOptions;
+	private MarginContainer Settings;
+
+	private bool isVisible = false;
+
+	public override void _Ready(){
+		Visible = isVisible;
+
+		ContinueButton = GetNode<Button>("PauseMenu/MainOptionsContainer/VBoxContainer/ContinueButton");
+		SaveButton = GetNode<Button>("PauseMenu/MainOptionsContainer/VBoxContainer/SaveButton");
+		SettingsButton = GetNode<Button>("PauseMenu/MainOptionsContainer/VBoxContainer/SettingsButton");
+		ExitButton = GetNode<Button>("PauseMenu/MainOptionsContainer/VBoxContainer/ExitButton");
+
+		MainOptions = GetNode<MarginContainer>("PauseMenu/MainOptionsContainer");
+		Settings = GetNode<MarginContainer>("PauseMenu/SettingsContainer");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta){
+		if(Input.IsActionJustPressed("Pause")){
+			Visible = !isVisible;
+		}
+	}
+
+	public void OnContinueButtonPressed(){
+		this.Visible = false;
+	}
+
+	public void OnSaveButtonPressed(){
+		EmitSignal(SignalName.SaveProgress);
+	}
+
+	public void OnSettingsButtonPressed(){
+		MainOptions.Visible = false;
+		Settings.Visible = true;
+	}
+
+	public void OnSettingsContainerOnGoBackClicked(){
+		MainOptions.Visible = true;
+		Settings.Visible = false;
+	}
+
+	public void OnExitButtonPressed(){
+		OnSaveButtonPressed();
+		GetTree().ChangeSceneToFile("res://scenes/MainMenu.tscn");
 	}
 }
