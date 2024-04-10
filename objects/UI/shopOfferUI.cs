be6fa48d932ec85e0de6a040741bb5d711fe0ed0
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-04-10 22:26:01 +0200

    dalsze tworzenie sklepu

--- objects/UI/shopOfferUI.cs
+++ objects/UI/shopOfferUI.cs
@@ -2,26 +2,53 @@ using Godot;
 using System;
 
 public partial class shopOfferUI : MarginContainer{
+	[Signal] public delegate void OnThingBoughtEventHandler();
+
 	[Export] private string ProductsName;
-	[Export] private string Description;
+	[Export] private string StorageName;
+	[Export] private string StorageCostName;
+	[Export] private string Type;
+	[Export] private int Value;
 	[Export] private string Action;
+	[Export] private Image Image;
+
+	DataReader dataReader;
 
 	private Label NameLabel;
-	private RichTextLabel DescriptionLabel;
+	private Label CostLabel;
 	private RichTextLabel ActionLabel;
+	public Label NumberOfOwned;
+	private ColorRect colorRect;
+	private TextureRect textureRect;
 	public override void _Ready(){
-		NameLabel = GetNode<Label>("HBoxContainer/VBoxContainer/Name");
-		DescriptionLabel = GetNode<RichTextLabel>("HBoxContainer/VBoxContainer/Description");
+		dataReader = (DataReader)GetNode("/root/DataReader");
+
+		NameLabel = GetNode<Label>("HBoxContainer/VBoxContainer/HBoxContainer/Name");
+		CostLabel = GetNode<Label>("HBoxContainer/VBoxContainer/HBoxContainer/Cost");
 		ActionLabel = GetNode<RichTextLabel>("HBoxContainer/VBoxContainer/Action");
+		colorRect = GetNode<ColorRect>("ColorRect");
+		NumberOfOwned = GetNode<Label>("HBoxContainer/Number");
+		textureRect = GetNode<TextureRect>("HBoxContainer/TextureRect");
 
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		NameLabel.Text = ProductsName;
+		ActionLabel.Text = Action;
+		CostLabel.Text = dataReader.ChosenSlot[StorageCostName];
+		if(Type == "tool") NumberOfOwned.Text = dataReader.ChosenSlot[StorageName];
+		else NumberOfOwned.Text = "";
 	}
 
 	public void OnPessed(){
+		int amountOfThings = Int32.Parse(dataReader.ChosenSlot[StorageName]);
+		int costOfThing = Int32.Parse(dataReader.ChosenSlot[StorageCostName]);
 
+		EmitSignal(SignalName.OnThingBought);
+		dataReader.ChangeData(StorageName, (amountOfThings + Value).ToString(), dataReader.ChosenSlotId);
+		dataReader.ChangeData("Money", (Int32.Parse(dataReader.ChosenSlot[StorageCostName])-costOfThing).ToString(), dataReader.ChosenSlotId);
+		if(Type == "tool"){
+			GD.Print("klil");
+			NumberOfOwned.Text = (amountOfThings + Value).ToString();
+			dataReader.ChangeData(StorageCostName, (costOfThing+costOfThing/20).ToString(), dataReader.ChosenSlotId);
+			CostLabel.Text = costOfThing+costOfThing/20 + "$";
+		} else Visible = false;
 	}
 }
