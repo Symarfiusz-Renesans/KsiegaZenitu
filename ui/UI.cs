b4d4e66602386ff525c919e241f9750a604c8bf0
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-04-16 21:25:07 +0200

    Revert "uproszczenie kodu kamery i dołożenie poprawki wizualnej"
    This reverts commit 164b206013920ca7b972773bea1e0832f60b47fc.

--- ui/UI.cs
+++ ui/UI.cs
@@ -49,29 +49,44 @@ public partial class UI : CanvasLayer
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta){
-		GameplayInfo.Visible = false;
-		Shop.Visible = false;
-		informationAboutClicks.Visible = false;
-
 		switch(angle){
 			case 0:{
+				GameplayInfo.Visible = false;
+				Shop.Visible = false;
 				informationAboutClicks.Visible = true;
 				informationAboutClicks.Text = "Clicks: "+amountOfClicksOnTheSun;
 				break;
 			}
 			case 90:{
-				Shop.Visible = true;
+				GameplayInfo.Visible = true;
+				Shop.Visible = false;
+				informationAboutClicks.Visible = false;
 				break;
 			}
 			case 180:{
+				GameplayInfo.Visible = false;
+				Shop.Visible = false;
 				informationAboutClicks.Visible = true;
 				informationAboutClicks.Text = "Clicks: "+amountOfClicksOnTheEarth;
 				break;
 			}
 			case 270:{
-				GameplayInfo.Visible = true;
+				Shop.Visible = true;
+				GameplayInfo.Visible = false;
+				informationAboutClicks.Visible = false;
 				break;
 			}
+		}
+
+		if(angle != 90){
+			GameplayInfo.Visible = false;
+		} 
+		if(angle != 270){
+			Shop.Visible = false;
+		}
+		if(angle == 0 || angle == 180){
+			GameplayInfo.Visible = false;
+			Shop.Visible = false;
 		}
 	}
 
